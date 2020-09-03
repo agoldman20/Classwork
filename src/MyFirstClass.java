f69bbf790c5a9f8e1a4d249de87f63dc6d1d605b
Goldman Abby <21goldman@st-21goldmanab.local>
2020-09-03 10:32:53 -0400

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,8 @@
+public class MyFirstClass {
+    public static void main(String args[])  {
+        char var = 'name'
+        name = 'Abby';
+        System.out.println("Dark theme is for the cool kids");
+    }
+
+}
